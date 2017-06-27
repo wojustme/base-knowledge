657383b321b4396968712fd00d961c35dec65454
wojustme <xurenhe19910131@163.com>
2017-06-27 18:17:46 +0800

    学习Quartz定时器

---
+++ scheduler/src/main/java/com/scheduler/HelloJob.java
@@ -0,0 +1,53 @@
+package com.scheduler;
+
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * ////////////////////////////////////////////////////////////////////
+ * //                          _ooOoo_                               //
+ * //                         o8888888o                              //
+ * //                         88" . "88                              //
+ * //                         (| ^_^ |)                              //
+ * //                         O\  =  /O                              //
+ * //                      ____/`---'\____                           //
+ * //                    .'  \\|     |//  `.                         //
+ * //                   /  \\|||  :  |||//  \                        //
+ * //                  /  _||||| -:- |||||-  \                       //
+ * //                  |   | \\\  -  /// |   |                       //
+ * //                  | \_|  ''\---/''  |   |                       //
+ * //                  \  .-\__  `-`  ___/-. /                       //
+ * //                ___`. .'  /--.--\  `. . ___                     //
+ * //              ."" '<  `.___\_<|>_/___.'  >'"".                  //
+ * //            | | :  `- \`.;`\ _ /`;.`/ - ` : | |                 //
+ * //            \  \ `-.   \_ __\ /__ _/   .-` /  /                 //
+ * //      ========`-.____`-.___\_____/___.-`____.-'========         //
+ * //                           `=---='                              //
+ * //      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^        //
+ * //             佛祖保佑       永无BUG     永不修改                   //
+ * ////////////////////////////////////////////////////////////////////
+ * <p>
+ * wojustme于2017/6/26祈祷...
+ */
+public class HelloJob implements Job {
+
+  public void execute(JobExecutionContext context)
+      throws JobExecutionException {
+    try {
+      Thread.sleep(5000L);
+    } catch (InterruptedException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+    // 打印当前的执行时间，格式为2017-01-01 00:00:00
+    Date date = new Date();
+    SimpleDateFormat sf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    System.out.println("Current Exec Time Is : " + sf.format(date));
+    System.out.println("Hello World");
+  }
+
+}
